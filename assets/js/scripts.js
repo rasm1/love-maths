85f35b843e574835ecfd18af85dd90c014680018
Mees R. <rasmees123@gmail.com>
2024-06-18 19:01:35 +0000

    Add js functionality to buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,30 +1,49 @@
-function runGame(){
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-}
-function checkAnswer(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-}
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
-
-}
-
-function incrementWrongAnswer(){
-
-}
-
-function displaysubtractQuestion(){
-
-}
-
-function displayMultiplyQuestion(){
+function runGame() {
 
 }
 
-function displayDivisionQuestion(){
+function checkAnswer() {
 
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
 }
